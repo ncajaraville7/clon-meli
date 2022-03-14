90b21c3da52af5e678cbd7a84e75b92617a2e6de
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2022-03-14 16:55:07 -0300

    add get detail

--- src/components/Products/ProductDetail.jsx
+++ src/components/Products/ProductDetail.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
+  const [detail, setDetail] = useState([]);
+  const { productId } = useParams();
+  // console.log(productId);
+
+  const getDetails = async () => {
+    const response = await fetch(
+      `https://api.mercadolibre.com/items/${productId}`
+    );
+    const data = await response.json();
+    setDetail(data);
+  };
+
+  useEffect(() => {
+    getDetails();
+  }, [productId]);
   return <div>ProductDetail</div>;
 };
 
--- src/components/Products/ProductListContainer.jsx
+++ src/components/Products/ProductListContainer.jsx
@@ -6,12 +6,18 @@ import './ProductListContainer.css';
 const ProductListContainer = ({ searchValue, items }) => {
   return (
     <div className="product-list">
-      <article className="product-list__container">
-        <ol className="product-list__container__items">
-          {items.length > 0 &&
-            items.map((item) => <Card key={item.id} {...item} />)}
-        </ol>
-      </article>
+      {items.length > 0 ? (
+        <article className="product-list__container">
+          <ol className="product-list__container__items">
+            {items.length > 0 &&
+              items.map((item) => <Card key={item.id} {...item} />)}
+          </ol>
+        </article>
+      ) : (
+        <div className="search-text">
+          <h2>Ingrese un producto...</h2>
+        </div>
+      )}
     </div>
   );
 };
